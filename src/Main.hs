d98d11b09c39530b44f0e3b16237ad2d26503e8c
Daniel Hilst Selli <danielhilst@gmail.com>
2021-09-05 22:30:46 -0300

    Use Vty

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import System.IO
 import Control.Concurrent
 import Control.Monad
 import Data.Array
@@ -8,6 +9,7 @@ import Data.Function
 import Debug.Trace
 import System.Console.ANSI (clearScreen)
 import qualified System.Process as SP
+import Graphics.Vty
 
 data Direction
   = DUp
@@ -16,14 +18,14 @@ data Direction
   | DRight
   deriving (Show)
 
-data Color
+data SquareColor
   = Black
   | White
   deriving (Show)
 
 type Pos = (Int, Int)
 
-type Universe = Array Pos Color
+type Universe = Array Pos SquareColor
 
 data Ant =
   Ant
@@ -32,13 +34,13 @@ data Ant =
     }
   deriving (Show)
 
-h = 50
+h = 100
 
-w = 100
+w = 200
 
 start = (h `div` 2, w `div` 2)
 
-mkArray :: (Int, Int) -> Array (Int, Int) Color
+mkArray :: (Int, Int) -> Array (Int, Int) SquareColor
 mkArray (maxx, maxy) =
   array
     ((0, 0), (maxy - 1, maxx - 1))
@@ -56,7 +58,7 @@ mkAnt =
 delay :: IO ()
 delay =
   let n = 3
-   in threadDelay (10000 * n)
+   in threadDelay (100 * n)
 
 -- clear terminal
 clear :: IO ()
@@ -64,15 +66,25 @@ clear = do
   _ <- SP.system "reset"
   return ()
 
-colorToChar :: Color -> Char
-colorToChar Black = '.'
-colorToChar White = '@'
+colorToChar :: SquareColor -> Char
+colorToChar Black = ' '
+colorToChar White = '\x25AE'
 
 printUniverse :: Universe -> IO ()
 printUniverse m = do
   forM_ (assocs m) $ \((y, x), cell) -> do
     putChar . colorToChar $ cell
     putChar '\n' & when (x == w - 1)
+
+part :: Int -> [a] -> [[a]]
+part _ [] = []
+part w xs = let (y, ys) = splitAt w xs
+    in y : part w ys
+
+renderUniverse :: Universe -> Image
+renderUniverse universe =
+  let linesColors = string defAttr <$> part w (colorToChar <$> elems universe)
+   in vertCat linesColors
 
 turnLeft :: Direction -> Direction
 turnLeft DUp = DLeft
@@ -86,21 +98,21 @@ turnRight DDown = DLeft
 turnRight DLeft = DUp
 turnRight DRight = DDown
 
-turnAnt :: Color -> Direction -> Direction
+turnAnt :: SquareColor -> Direction -> Direction
 turnAnt White dir = turnRight dir
 turnAnt Black dir = turnLeft dir
 
-flipColor :: Color -> Color
-flipColor Black = White
-flipColor White = Black
+flipSquareColor :: SquareColor -> SquareColor
+flipSquareColor Black = White
+flipSquareColor White = Black
 
-updateCell :: Pos -> Color -> Universe -> Universe
+updateCell :: Pos -> SquareColor -> Universe -> Universe
 updateCell (y, x) color universe = universe // [((y, x), color)]
 
 flipCell :: Pos -> Universe -> Universe
 flipCell (y, x) universe =
   let c = universe ! (y, x)
-   in updateCell (y, x) (flipColor c) universe
+   in updateCell (y, x) (flipSquareColor c) universe
 
 moveForward :: Direction -> (Int, Int) -> (Int, Int)
 moveForward DLeft (y, x) = (y, (x - 1) `mod` w)
@@ -108,13 +120,13 @@ moveForward DRight (y, x) = (y, (x + 1) `mod` w)
 moveForward DUp (y, x) = ((y - 1) `mod` h, x)
 moveForward DDown (y, x) = ((y + 1) `mod` h, x)
 
-moveAnt :: Color -> Ant -> Ant
+moveAnt :: SquareColor -> Ant -> Ant
 moveAnt currentColor Ant {pos = (y, x), dir = dir} =
   let newDir = turnAnt currentColor dir
       (x', y') = moveForward newDir (y, x)
    in Ant {pos = (x', y'), dir = newDir}
 
-getCurrentCellColor :: Ant -> Universe -> Color
+getCurrentCellColor :: Ant -> Universe -> SquareColor
 getCurrentCellColor Ant {pos = (y, x)} universe = universe ! (y, x)
 
 stepSystem :: (Ant, Universe) -> (Ant, Universe)
@@ -123,13 +135,29 @@ stepSystem (ant@Ant {pos = pos}, universe) =
       newAnt = moveAnt currentCellColor ant
    in (newAnt, flipCell pos universe)
 
-runSystem :: (Ant, Universe) -> Int -> IO ()
-runSystem system@(ant, universe) step = do
+asciiRender :: Universe -> IO ()
+asciiRender universe = do
   clearScreen
   printUniverse universe
-  print $ "Step: " ++ show step
+  --print $ "Step: " ++ show step
+  hFlush stdout
   delay
-  runSystem (stepSystem system) (step + 1)
+
+asciiInit :: IO ()
+asciiInit = do
+  hSetBuffering stdout $ BlockBuffering (Just (w * h))
+
+vtyRender :: Vty -> Universe -> IO ()
+vtyRender vty universe = do
+  update vty (picForImage (renderUniverse universe))
+
+runSystem :: (Universe -> IO ()) -> (Ant, Universe) -> Int -> IO ()
+runSystem render system@(ant, universe) step = do
+  render universe
+  runSystem render (stepSystem system) (step + 1)
 
 main :: IO ()
-main = runSystem (mkAnt, mkUniverse) 0
+main = do
+  cfg <- standardIOConfig
+  vty <- mkVty cfg
+  runSystem (vtyRender vty) (mkAnt, mkUniverse) 0
